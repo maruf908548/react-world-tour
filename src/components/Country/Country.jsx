8247fc3c84279dbb14420e8fb7be7c4ea6408a99
maruf908548 <ahmedmaruf908548@gmail.com>
2024-09-25 13:57:31 +0600

    country code added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,10 +1,15 @@
 import "./Country.css";
 const Country = ({ country }) => {
-  const { name, flags } = country;
+  const { name, flags, population, area, cca3 } = country;
   return (
     <div className="country">
       <h3>Name: {name?.common}</h3>
       <img src={flags.png} alt="" />
+      <p>population: {population}</p>
+      <p>area: {area}</p>
+      <p>
+        <small>Code: {cca3}</small>
+      </p>
     </div>
   );
 };
